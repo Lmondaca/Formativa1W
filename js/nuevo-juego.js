6e942afebfce7bf88806efc3b01abac3e3b7129f
Nezkh <f.marchant.aravena@gmail.com>
2025-03-31 19:24:22 -0300

    form nuevo juego y boton admin

--- js/nuevo-juego.js
+++ js/nuevo-juego.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
     $('#form_nuevojuego').submit(function(event) {
         event.preventDefault();
         $('.text-danger').text('');
-        
+
+
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/webp'];
+        const maxSizeMB = 2;
+        const maxSizeBytes = maxSizeMB * 1024 * 1024;
+
         const categoria = $('#categoria').val();
         const nombre = $('#nombre').val().trim();
         const descripcion = $('#descripcion').val().trim();
@@ -34,11 +39,20 @@ $(document).ready(function() {
         if (!imagen) {
             $('#imagen-error').text('Selecciona una imagen');
             hasErrors = true;
+        } else {
+            if (!allowedTypes.includes(imagen.type)) {
+                $('#imagen-error').text('Formato no válido. Use JPG, PNG o WEBP');
+                hasErrors = true;
+            }
+            
+            if (imagen.size > maxSizeBytes) {
+                $('#imagen-error').text(`El tamaño máximo permitido es ${maxSizeMB}MB`);
+                hasErrors = true;
+            }
         }
 
         if (hasErrors) return;
 
-        // convertir imagen a Base64
         const reader = new FileReader();
         reader.onload = function(e) {
             const newGame = {
