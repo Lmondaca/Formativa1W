d6274cba02439193806219cff59889345d55fda1
Lucas Mondaca <lucas.mondaca@hotmail.com>
2025-05-05 15:31:40 -0400

    modificacion llave foranea modelo

--- fastapi/gamelfapi/models.py
+++ fastapi/gamelfapi/models.py
@@ -3,25 +3,25 @@ from .database import Base
 
 # Oracle
 
-juego_seq = Sequence("juego_seq", start=1, increment=1)
-categoria_seq = Sequence("categoria_seq", start=1, increment=1)
+
 
 class Categoria(Base):
     __tablename__ = "catalagos_categoria"
-    
+    categoria_seq = Sequence("categoria_seq", start=1, increment=1)
+    juego_seq = Sequence("juego_seq", start=1, increment=1)
     idCategoria = Column(Integer, categoria_seq, primary_key=True, server_default=categoria_seq.next_value())
     nombre = Column(String(50), nullable=False)
     descripcion = Column(String(200))
 
 class Juegos(Base):
     __tablename__ = "catalagos_juego"
-
+    juego_seq = Sequence("juego_seq", start=1, increment=1)
     idjuego = Column(Integer, juego_seq, primary_key=True, server_default=juego_seq.next_value())
     nombre = Column(String(60), index=True)
     descripcion = Column(String(1000))
     precio = Column(Integer)
     image = Column(String(100))
-    categoria_id = Column(Integer, ForeignKey("categoria.idCategoria"))
+    categoria_id = Column(Integer, ForeignKey("catalagos_categoria.idCategoria"))
     imagen = Column(String(100))
     
     
