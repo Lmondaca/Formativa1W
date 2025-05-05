d251c8245bb30ca238be0eb481428520cebebf5c
Lucas Mondaca <lucas.mondaca@hotmail.com>
2025-05-05 14:57:40 -0400

    modificacion fastpai

--- fastapi/gamelfapi/models.py
+++ fastapi/gamelfapi/models.py
@@ -3,39 +3,41 @@ from .database import Base
 
 # Oracle
 
-# juego_seq = Sequence("juego_seq", start=1, increment=1)
-# categoria_seq = Sequence("categoria_seq", start=1, increment=1)
+juego_seq = Sequence("juego_seq", start=1, increment=1)
+categoria_seq = Sequence("categoria_seq", start=1, increment=1)
 
+class Categoria(Base):
+    __tablename__ = "catalagos_categoria"
+    
+    idCategoria = Column(Integer, categoria_seq, primary_key=True, server_default=categoria_seq.next_value())
+    nombre = Column(String(50), nullable=False)
+    descripcion = Column(String(200))
+
+class Juegos(Base):
+    __tablename__ = "catalagos_juego"
+
+    idjuego = Column(Integer, juego_seq, primary_key=True, server_default=juego_seq.next_value())
+    nombre = Column(String(60), index=True)
+    descripcion = Column(String(1000))
+    precio = Column(Integer)
+    image = Column(String(100))
+    categoria_id = Column(Integer, ForeignKey("categoria.idCategoria"))
+    imagen = Column(String(100))
+    
+    
+# SQLite
 # class Categoria(Base):
 #     __tablename__ = "categoria"
     
-#     idCategoria = Column(Integer, categoria_seq, primary_key=True, server_default=categoria_seq.next_value())
+#     idCategoria = Column(Integer, primary_key=True)
 #     nombre = Column(String(50), nullable=False)
 #     descripcion = Column(String(200))
 
 # class Juegos(Base):
 #     __tablename__ = "juegos"
 
-#     id = Column(Integer, juego_seq, primary_key=True, server_default=juego_seq.next_value())
+#     id = Column(Integer, primary_key=True)
 #     nombre = Column(String(60), index=True)
 #     descripcion = Column(String(1000))
 #     precio = Column(Integer)
-#     categoria_id = Column(Integer, ForeignKey("categoria.idCategoria"))
-    
-    
-# SQLite
-class Categoria(Base):
-    __tablename__ = "categoria"
-    
-    idCategoria = Column(Integer, primary_key=True)
-    nombre = Column(String(50), nullable=False)
-    descripcion = Column(String(200))
-
-class Juegos(Base):
-    __tablename__ = "juegos"
-
-    id = Column(Integer, primary_key=True)
-    nombre = Column(String(60), index=True)
-    descripcion = Column(String(1000))
-    precio = Column(Integer)
-    categoria_id = Column(String, ForeignKey("categoria.idCategoria"))
+#     categoria_id = Column(String, ForeignKey("categoria.idCategoria"))
--- fastapi/gamelfapi/schema.py
+++ fastapi/gamelfapi/schema.py
@@ -4,13 +4,13 @@ class JuegosBase(BaseModel):
     nombre: str
     descripcion: str
     precio: int
-    categoria_id: str
+    categoria_id: int
     
 class JuegosCreate(JuegosBase):
     pass
 
 class Juegos(JuegosBase):
-    id: int
+    idjuego: int
     
     class Config:
         from_attributes = True
